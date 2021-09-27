75f642716454512042c9f82e6d3b36f72308bd2b
YQiu-oo <1076878095qyk@gmail.com>
2021-09-27 16:28:44 -0500

    create a user page

--- Research/yqiu30/app.py
+++ Research/yqiu30/app.py
@@ -1,12 +1,16 @@
 from flask import Flask
-
+from flask import render_template
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "<b>Hello,this is the main page</b>"
-@app.route("/Hello")      
-def hello():
-    return "hello hello I am Yukang"
+    return "<h1>Welcome to the Login Page</h1>"
+@app.route("/login", methods=["POST", "GET"])      
+def login():
+    return render_template('login.html')
+
+@app.route("/<user>")
+def user(user):
+    return "<h1>{user}</h1>"
 if __name__ == "__main__":
     app.run()
 
