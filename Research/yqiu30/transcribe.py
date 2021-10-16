347619935b3337949134106896fd48970c22a13b
YQiu-oo <1076878095qyk@gmail.com>
2021-10-15 23:30:42 -0500

    the first version of extracting audio from video

--- Research/yqiu30/transcribe.py
+++ Research/yqiu30/transcribe.py
@@ -1,7 +1,27 @@
+from moviepy.config import try_cmd
 import speech_recognition as sr
-r = sr.Recognizer()
+import moviepy.editor
+video = moviepy.editor.VideoFileClip('file.mp4')
+au = video.audio
+str = "file.wav"
+aa = au.write_audiofile(str)
 
-job = sr.AudioFile("harvard.wav")
-with job as source:
-    audio = r.record(source)
-print(r.recognize_google(audio))
+
+def main():
+    sound = str
+    r = sr.Recognizer()
+
+    with sr.AudioFile(sound) as source:
+        r. adjust_for_ambient_noise(source)
+        print("Converting Audio File to Text------")
+        audio = r.record(source)
+
+        try:
+            print("Converted Audio is " + r.recognize_google(audio))
+
+        except Exception as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    main()
