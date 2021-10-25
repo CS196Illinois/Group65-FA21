9f43429d6e0ca90a1bec9397e52a13c76bcb7507
albearli <yiliang6@illinois.edu>
2021-10-25 15:36:35 -0700

    Update website.jsx

--- Research/qouyang3/my-app/src/components/website.jsx
+++ Research/qouyang3/my-app/src/components/website.jsx
@@ -1,30 +1,29 @@
-import React, { Component, useState } from "react";
+import React from "react";
 
-class website extends React.Component {
-  render() {
-    const styleTitle = {
-      fontSize: 36,
-      color: "#4a54f1",
-      textAlign: "center",
-      paddingTop: "100px",
-    };
-    const centering = {
-      textAlign: "center",
-    };
-    return (
-      <div>
-        <h1 style={styleTitle}>LECTURE VIDEOS => SUMMARIZED TRANSCRIPTS</h1>
-        <h3 style={centering}>
-          Speed-running recorded lectures not good enough for you? Get a clean,
-          readable summary of it instead!
-        </h3>
-        <button className="btn m-4 btn-primary btn-lg">Upload</button>
-        <p style={centering}>X minutes of videos uploaded!</p>
-        <p style={centering}>X minutes of words removed!</p>
-        <p style={centering}>X% summary satisfaction rate!</p>
-      </div>
-    );
-  }
+
+export default function Website() {
+  const styleTitle = {
+    fontSize: 36,
+    color: "#4a54f1",
+    textAlign: "center",
+    paddingTop: "100px",
+  };
+
+  const centering = {
+    textAlign: "center",
+  };
+
+  return (
+    <div style={centering}>
+      <h1 style={styleTitle}>LECTURE VIDEOS =&gt; SUMMARIZED TRANSCRIPTS</h1>
+      <h3>
+        Speed-running recorded lectures not good enough for you? Get a clean,
+        readable summary of it instead!
+      </h3>
+      <button className="btn m-4 btn-primary btn-lg">Upload</button>
+      <p>X minutes of videos uploaded!</p>
+      <p>X minutes of words removed!</p>
+      <p>X% summary satisfaction rate!</p>
+    </div>
+  );
 }
-
-export default website;
