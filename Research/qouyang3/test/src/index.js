0f5a8d9b64e357b20a59fc543f4fb7d14168e1a0
quinnouyang <quinn.ouy@gmail.com>
2021-10-25 14:55:10 -0500

    DO NOT PUSH TO MASTER
    DO NOT PUSH TO MASTER
    Progress on React tutorials, except this should be in a non-conflicting folder

--- Research/qouyang3/test/src/index.js
+++ Research/qouyang3/test/src/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import Counter from "./components/counter";
 
 const element = <h1>Hello World</h1>;
 
-ReactDOM.render(element, document.getElementById('root'))
+ReactDOM.render(<Counter />, document.getElementById("root"));
