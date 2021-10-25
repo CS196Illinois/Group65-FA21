c0a6f97094ddb88aa85d028a298abb42d3e7e235
quinnouyang <quinn.ouy@gmail.com>
2021-10-25 17:05:14 -0500

    some website homepage implementation
    files are messy and jank, gotta reorganize without destroying everything

--- Research/qouyang3/my-app/src/components/website.jsx
+++ Research/qouyang3/my-app/src/components/website.jsx
@@ -2,7 +2,28 @@ import React, { Component } from "react";
 
 class website extends React.Component {
   render() {
-    return <div>hahahahah</div>;
+    const styleTitle = {
+      fontSize: 36,
+      color: "#4a54f1",
+      textAlign: "center",
+      paddingTop: "100px",
+    };
+    const centering = {
+      textAlign: "center",
+    };
+    return (
+      <div>
+        <h1 style={styleTitle}>LECTURE VIDEOS => SUMMARIZED TRANSCRIPTS</h1>
+        <h3 style={centering}>
+          Speed-running recorded lectures not good enough for you? Get a clean,
+          readable summary of it instead!
+        </h3>
+        <button className="btn m-4 btn-primary btn-sm">Upload</button>
+        <p style={centering}>X minutes of videos uploaded!</p>
+        <p style={centering}>X minutes of words removed!</p>
+        <p style={centering}>X% summary satisfaction rate!</p>
+      </div>
+    );
   }
 }
 
