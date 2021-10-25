a0b461abf50868832c25c0a139f28a6ed25ea519
quinnouyang <quinn.ouy@gmail.com>
2021-10-25 16:35:50 -0500

    moved a bunch of stuff

--- Research/qouyang3/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.css";
-import Counters from "./components/counters";
-
-const element = <h1>Hello World</h1>;
-
-ReactDOM.render(<Counters />, document.getElementById("root"));
