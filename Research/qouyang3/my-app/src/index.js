3378961a6e86f180bf522d9fae6b4cfe129d553a
quinnouyang <quinn.ouy@gmail.com>
2021-10-25 19:28:19 -0500

    minor edits

--- Research/qouyang3/my-app/src/index.js
+++ Research/qouyang3/my-app/src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import Website from "./components/website";
+import Counters from "./components/counters";
 
-ReactDOM.render(<Website />, document.getElementById("root"));
+ReactDOM.render(<Counters />, document.getElementById("root"));
