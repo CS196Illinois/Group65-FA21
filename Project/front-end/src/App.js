1ccc147518e0ea147cd9ac40f4d5b8224e6c4b78
quinnouyang <quinn.ouy@gmail.com>
2021-11-29 17:15:41 -0600

    Testing Routing to Different Components
    broken lol

--- Project/front-end/src/App.js
+++ Project/front-end/src/App.js
@@ -1,69 +1,28 @@
 import React from "react";
-import { ReactComponent as Logo } from "./figure.svg";
+import { render } from "react-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useRoutes,
+} from "react-router-dom";
+import Home from "./Home";
+import Test from "./Test";
 
-const sideBySideContainer = {
-  display: "flex",
-};
-
-const styleTitle1 = {
-  fontSize: 72,
-  paddingTop: "100px",
-  fontWeight: "bold",
-  // paddingRight: "700px",
-};
-const styleTitle2 = {
-  fontSize: 72,
-  paddingTop: "0px",
-  // paddingRight: "700px",
-  fontWeight: "bold",
-  fontStyle: "italic",
-};
-const styleSubtitle = {
-  fontSize: 24,
-  textAlign: "right",
-  // paddingRight: "700px",
+const App = () => {
+  let routes = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "test", element: <Test /> },
+  ]);
+  return routes;
 };
 
-const centering = {
-  textAlign: "center",
-  height: "100%",
-  position: "relative",
-  minHeight: "100vh",
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  justifyContent: "center",
-};
-
-const right = {
-  textAlign: "right",
-  paddingRight: "50px",
-};
-
-const styleStats = {
-  fontSize: 24,
-};
-
-export default function App() {
+const AppWrapper = () => {
   return (
-    <div style={centering}>
-      <div style={sideBySideContainer}>
-        <div style={right}>
-          <h1 style={styleTitle1}>Lecture Videos...</h1>
-          <h1 style={styleTitle2}>Summarized.</h1>
-          <h3 style={styleSubtitle}>
-            Can’t understand your lectures at ×2 speed?
-          </h3>
-          <h3 style={styleSubtitle}>
-            Get a shorter and readable transcript instead!
-          </h3>
-        </div>
-        <Logo />
-      </div>
-      <button className="btn m-4 btn-primary btn-lg">Upload</button>
-      <p style={styleStats}>X minutes of videos uploaded!</p>
-      <p style={styleStats}>X minutes of words removed!</p>
-      <p style={styleStats}>X% summary satisfaction rate!</p>
-    </div>
+    <Router>
+      <App />
+    </Router>
   );
-}
+};
+
+render(<AppWrapper />, document.getElementById("root"));
--- Project/front-end/src/index.js
+++ Project/front-end/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css";
+// import App from "./App";
+import Home from "./Home";
+// import "bootstrap/dist/css/bootstrap.css";
+import "./index2.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Home />, document.getElementById("root"));
