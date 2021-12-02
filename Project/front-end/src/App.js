7367caf01461fc15c908f5fbdd1095022196d083
quinnouyang <quinn.ouy@gmail.com>
2021-12-02 16:08:28 -0600

    TS Install

--- Project/front-end/src/App.js
+++ Project/front-end/src/App.js
@@ -3,14 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Test from "./Test";
 
-// const App = () => {
-//   let routes = useRoutes([
-//     { path: "/", element: <Home /> },
-//     { path: "test", element: <Test /> },
-//   ]);
-//   return routes;
-// };
-
 export default function AppWrapper() {
   return (
     <Router>
