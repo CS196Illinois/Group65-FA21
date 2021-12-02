5fff07fd1dbe94c14704e40d607b8ee5cca91039
Varun Mittal <varunmittal2002@gmail.com>
2021-12-02 15:16:43 -0600

    speechrecog

---
+++ Research/varunm3/speechrecog.py
@@ -0,0 +1,9 @@
+import speech_recognition as sr
+
+filename = "audio.mp3"
+r = sr.Recognizer()
+
+with sr.AudioFile(filename) as source:
+    audio_data = r.record(source)
+    text = r.recognize_google(audio_data)
+    print(text)
