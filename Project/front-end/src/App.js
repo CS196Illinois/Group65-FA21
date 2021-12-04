e76ff4c5f94f01d798bc6ba7c87aaddf47738058
quinnouyang <quinn.ouy@gmail.com>
2021-12-04 14:30:48 -0600

    Implemented Form Input, Reorganized Files, etc.

--- Project/front-end/src/App.js
+++ Project/front-end/src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Home2";
-import Test from "./Test";
+import Home from "./Home";
+import Result from "./Result";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Test" element={<Test />} />
-        <Route path="/Home2" element={<Home />} />
+        <Route path="/Result" element={<Result />} />
+        <Route path="/Home" element={<Home />} />
       </Routes>
     </Router>
   );
