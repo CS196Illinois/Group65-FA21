a18c445f2c19b166eb77044a2074d66bc5a797fb
quinnouyang <quinn.ouy@gmail.com>
2022-05-14 16:17:29 -0500

    Sync

---
+++ Project/back-end/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+import json
+
+from transcriber import transcribe
+from summarizer import summarize
+
+app = Flask(__name__)
+cors = CORS(app)
+
+@app.route("/transcriber/<file>")
+def generate(file):
+    transcript  = transcribe(file)
+    print("Transciption - Done.")
+    data = {
+        "transcript": transcript,
+        "summary": summarize(transcript)
+        # "summary": transcript,
+    }
+    print("Summary - Done.")
+    return json.dumps(data)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=1234)
